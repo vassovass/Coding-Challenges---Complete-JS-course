d7e3b50b5bfc7440eb72bafc1c8d66e0eda31299
Vasso Vassiliades <vaso@vaseo.co.za>
2022-08-04 23:53:05 +0200

    S3CH1 - Correction made ot code to  be absolutely correct

--- S3 JS Fundamentals Part 2 - Challenge 1/index.js
+++ S3 JS Fundamentals Part 2 - Challenge 1/index.js
@@ -41,12 +41,12 @@ const checkWinner = (avgDolphins, avgKoalas) => {
   } else {
     console.log(`nobody wins (${avgDolphins} vs. ${avgKoalas})`)
   }
-  return checkWinner
+
 }
-console.log(checkWinner(avgDolphins, avgKoalas))
+checkWinner(avgDolphins, avgKoalas)
 
 avgDolphins = calcAverage(85, 54, 41);
 avgKoalas = calcAverage(23, 34, 27);
-console.log(checkWinner(avgDolphins, avgKoalas))
+checkWinner(avgDolphins, avgKoalas)
 
 // todo Check this against answer in tut, but read it first and see why undefined is shown - got rid of undefined - but logging whole if statement - need to fix
